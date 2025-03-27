20a01ed922f40a5243216cfbeac026a0e13682c5
shriKishan-21 <punia21ashu@gmail.com>
2025-03-27 21:54:07 +0530

    second

--- Array/BASIC/SLIDING_WINDOW_fixed.cpp
+++ Array/BASIC/SLIDING_WINDOW_fixed.cpp
@@ -1,9 +1,43 @@
-// // SLIDING WINDOW FIXED SIZE
+// SLIDING WINDOW FIXED SIZE
 
-// #include <iostream>
-// using namespace std;
-// int main()
-// {
-//     int arr[]=
-//     return 0;
-// }
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to compute the maximum sum of a fixed-size sliding window
+int maxSlidingWindowSum(vector<int> &arr, int k)
+{
+    if (arr.size() < k)
+    {
+        cout << "Window size is larger than array size!" << endl;
+        return -1;
+    }
+
+    int maxSum = 0, windowSum = 0;
+
+    // Compute the sum of the first window
+    for (int i = 0; i < k; i++)
+    {
+        windowSum += arr[i];
+    }
+    maxSum = windowSum;
+
+    // Slide the window through the array
+    for (int i = k; i < arr.size(); i++)
+    {
+        windowSum += arr[i] - arr[i - k]; // Add new element, remove old element
+        maxSum = max(maxSum, windowSum);
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+    vector<int> arr = {1, 3, 5, 7, 9, 2, 6, 4, 8};
+    int k = 3; // Window size
+
+    int result = maxSlidingWindowSum(arr, k);
+    cout << "Maximum sum of any window of size " << k << " is: " << result << endl;
+
+    return 0;
+}
--- Array/BASIC/leetcode_238.cpp
+++ Array/BASIC/leetcode_238.cpp
@@ -1,74 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int calculate(vector<int> &nums, int num)
-{
-    int zero = 0;
-    int j = 0;
-    int product = 1;
-    for (int i = 0; i < nums.size(); i++)
-    {
-        if (nums[i] == 0)
-        {
-            zero++;
-        }
-    }
-    if (zero > 1)
-    {
-        return product = 0;
-    }
-    else
-    {
-
-        while (j < nums.size())
-        {
-            if (nums[j] == num && j < nums.size())
-            {
-                j++;
-            }
-            if (j < nums.size())
-            {
-                product = product * nums[j];
-            }
-            j++;
-        }
-        return product;
-    }
-}
 
 vector<int> productExceptSelf(vector<int> &nums)
 {
-    vector<int> ans;
-    for (int i = 0; i < nums.size(); i++)
+    int n = nums.size();
+    vector<int> result(n, 1);
+
+    // Compute prefix products
+    int prefix = 1;
+    for (int i = 0; i < n; i++)
     {
-        ans.push_back(calculate(nums, nums[i]));
+        result[i] = prefix;
+        prefix *= nums[i];
     }
-    return ans;
+
+    // Compute suffix products and multiply with prefix
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return result;
 }
-
-// vector<int> productExceptSelf(vector<int> &nums)
-// {
-//     int n = nums.size();
-//     vector<int> result(n, 1);
-
-//     // Compute prefix products
-//     int prefix = 1;
-//     for (int i = 0; i < n; i++)
-//     {
-//         result[i] = prefix;
-//         prefix *= nums[i];
-//     }
-
-//     // Compute suffix products and multiply with prefix
-//     int suffix = 1;
-//     for (int i = n - 1; i >= 0; i--)
-//     {
-//         result[i] *= suffix;
-//         suffix *= nums[i];
-//     }
-
-//     return result;
-// }
 
 int main()
 {
