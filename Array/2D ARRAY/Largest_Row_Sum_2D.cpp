890a624cd19c50f73477dd7344b626fc39e8a2f9
shriKishan-21 <punia21ashu@gmail.com>
2025-05-05 00:08:30 +0530

    basics

--- Array/2D ARRAY/Largest_Row_Sum_2D.cpp
+++ Array/2D ARRAY/Largest_Row_Sum_2D.cpp
@@ -1,8 +1,8 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
 
-largestSum(int arr[][4],int )
-int main(){
+// largestSum(int arr[][4],int )
+// int main(){
 
-}
+// }
--- Array/BASIC/121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ Array/BASIC/121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,63 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int min(vector<int> &prices)
-{
-    int mini = prices[0];
-    for (int i = 0; i < prices.size(); i++)
-    {
-        if (prices[i] < mini)
-        {
-            mini = prices[i];
-        }
-    }
-    return mini;
-}
-int max(vector<int> &prices, int index)
-{
-    int maxi = prices[index];
-    for (int i = index; i < prices.size(); i++)
-    {
-        if (prices[i] > maxi)
-        {
-            maxi = prices[i];
-        }
-    }
-    return maxi;
-}
 int maxProfit(vector<int> &prices)
 {
-    int buy = min(prices), sell = 0;
-    cout<<"buy: "<<buy<<endl;
-    int index = 0;
-    for (int i = 0; i < prices.size(); i++)
+    int n = prices.size();
+    int mini = prices[0], res = 0;
+    for (int i = 0; i < n; i++)
     {
-        if (prices[i] == buy)
-        {
-            index = i;
-            break;
-        }
+        mini = min(mini, prices[i]);
+
+        res = max(res, prices[i] - mini);
     }
-    cout<<"index: "<<index<<endl;
-    if (index == prices.size() - 1)
-    {
-        return 0;
-    }
-    else
-    {
-        sell = max(prices, index);
-    }
-    cout<<"sell: "<<sell<<endl;
-    if (sell - buy <= 0)
-    {
-        return 0;
-    }
-    return sell - buy;
+    return res;
 }
 
-int main(){
-    vector<int> arr={7,1,5,3,6,4};
-    cout<<maxProfit(arr);
-    cout<<endl;
-    cout<<max(arr,1);
+int main()
+{
+    vector<int> arr = {7, 1, 5, 3, 6, 4};
+    cout << maxProfit(arr);
 }
